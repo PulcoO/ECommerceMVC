dcb17c0b6f0effa3148b2f90b403cbd20b00dd60
PulcoOx <oominimathoo@gmail.com>
2020-08-18 10:12:08 +0200

    add identity
    bug

---
+++ ECommerceMVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ECommerceMVC.Data;
+using ECommerceMVC.Models.Clients;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(ECommerceMVC.Areas.Identity.IdentityHostingStartup))]
+namespace ECommerceMVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
